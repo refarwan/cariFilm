5afdbf3105ef7a55e6747d4f2549210148c48e48
Aan Refarwan <refarwanaan@gmail.com>
2024-01-20 23:18:39 +0700

    finishing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
-import Image from "next/image"
+"use client"
+
+import { FormEvent, useRef } from "react"
+import { useRouter } from "next/navigation"
+
+import { FaPlayCircle, FaSearch } from "react-icons/fa"
 
 export default function Home() {
-	return <main></main>
+	const searchRef = useRef<HTMLInputElement>(null)
+	const router = useRouter()
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault()
+		router.push(`search/${searchRef.current?.value}`)
+	}
+
+	return (
+		<main className="grid place-content-center w-screen h-screen gap-[10px] p-[30px]">
+			<div className="text-teal-700 flex gap-[10px] items-center justify-center">
+				<FaPlayCircle size={30} />
+				<span className="text-[30px]">
+					Cari<span className="font-semibold">FILM</span>
+				</span>
+			</div>
+			<form
+				onSubmit={handleSubmit}
+				className="border border-gray-300 h-[40px] w-[315px] rounded-[6px] flex justify-between px-[12px]"
+			>
+				<input
+					ref={searchRef}
+					name="search"
+					type="text"
+					className="outline-none"
+				/>
+				<button type="submit" className="text-teal-700">
+					<FaSearch size={20} />
+				</button>
+			</form>
+		</main>
+	)
 }
