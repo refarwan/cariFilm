b5a42cf8a132222cb1a9fc2c8745239dc24b4c1f
Aan Refarwan <refarwanaan@gmail.com>
2024-01-21 17:34:34 +0700

    add pagination

--- src/app/search/[type]/[search]/MovieList.tsx
+++ src/app/search/[type]/[search]/MovieList.tsx
@@ -8,9 +8,10 @@ export const dynamic = "force-dynamic"
 type propsType = {
 	search: string
 	type: "all" | "movie" | "series" | "game"
+	page?: number
 }
 
-const MovieList = async ({ search, type = "all" }: propsType) => {
+const MovieList = async ({ search, type = "all", page = 1 }: propsType) => {
 	try {
 		type responseType = {
 			Search: {
@@ -24,37 +25,60 @@ const MovieList = async ({ search, type = "all" }: propsType) => {
 			Response: "True" | "False"
 		}
 
-		const response = await axios.get(
+		const responseAPI = await axios.get(
 			`${process.env.NEXT_OMDB_API_URL}/?apikey=${
 				process.env.NEXT_API_KEY
-			}&s=${search}${type !== "all" ? `&type=${type}` : ""}&page=1`
+			}&s=${search}${type !== "all" ? `&type=${type}` : ""}&page=${page}`
 		)
 
-		const data: responseType = response.data
+		const data: responseType = responseAPI.data
 
 		return (
 			<>
 				{data.Response === "True" ? (
-					data.Search.map((item, index) => (
-						<Link
-							href={`/detail/${item.imdbID}`}
-							key={index}
-							className="w-[150px] flex flex-col gap-[10px]"
-						>
-							{item.Poster && item.Poster !== "N/A" ? (
-								<img
-									className="w-[150px]"
-									src={item.Poster}
-									alt={`Poster ${item.Title}`}
-								/>
-							) : (
-								<div className="w-[150px] h-[200px] bg-gray-300 overflow-hidden grid place-content-center text-gray-400">
-									<FaFilm size={50} />
-								</div>
-							)}
-							<span>{item.Title}</span>
-						</Link>
-					))
+					<>
+						{data.Search.map((item, index) => (
+							<Link
+								href={`/detail/${item.imdbID}`}
+								key={index}
+								className="w-[150px] flex flex-col gap-[10px]"
+							>
+								{item.Poster && item.Poster !== "N/A" ? (
+									<img
+										className="w-[150px]"
+										src={item.Poster}
+										alt={`Poster ${item.Title}`}
+									/>
+								) : (
+									<div className="w-[150px] h-[200px] bg-gray-300 overflow-hidden grid place-content-center text-gray-400">
+										<FaFilm size={50} />
+									</div>
+								)}
+								<span>{item.Title}</span>
+							</Link>
+						))}
+						{parseInt(data.totalResults) / 10 > 1 ? (
+							<div className="m-auto col-span-2 sm:col-span-3 xl:col-span-5 flex gap-[20px]">
+								{page > 1 ? (
+									<Link
+										className="bg-teal-700 text-white inline-block px-[24px] h-[40px] rounded-[6px] leading-[40px] text-center"
+										href={`/search/${type}/${search}/${page - 1}`}
+									>
+										Previous Page
+									</Link>
+								) : null}
+								{data.Search.length + page * 10 <
+								parseInt(data.totalResults) ? (
+									<Link
+										className="bg-teal-700 text-white inline-block px-[24px] h-[40px] rounded-[6px] leading-[40px] text-center"
+										href={`/search/${type}/${search}/${page + 1}`}
+									>
+										Next Page
+									</Link>
+								) : null}
+							</div>
+						) : null}
+					</>
 				) : (
 					<p className="font-semibold text-[20px] text-gray-400 text-center col-span-2 sm:col-span-3 xl:col-span-5">
 						Tidak ada data ditemukan
---
+++ src/app/search/[type]/[search]/[page]/page.tsx
@@ -0,0 +1,32 @@
+import { Suspense } from "react"
+
+import Header from "@/components/Header"
+import Loading from "@/components/Loading"
+import MovieList from "../MovieList"
+
+type paramsType = {
+	params: {
+		type: "all" | "movie" | "series" | "game"
+		search: string
+		page: string
+	}
+}
+
+const page = ({ params }: paramsType) => {
+	return (
+		<>
+			<Header search={params.search} searchType={params.type} />
+			<main className="w-max mt-[91px] p-[25px] grid grid-cols-2 gap-[25px] m-auto sm:mt-[101px] sm:p-[40px] sm:grid-cols-3 xl:mt-[73px] xl:grid-cols-5 xl:gap-[40px]">
+				<Suspense fallback={<Loading />}>
+					<MovieList
+						search={params.search}
+						type={params.type}
+						page={parseInt(params.page)}
+					/>
+				</Suspense>
+			</main>
+		</>
+	)
+}
+
+export default page
